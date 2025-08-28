230e64a21363ada2c3c747d70492a5ccd95e5172
Ignacio Noguerol <ignacionogpa@gmail.com>
2025-08-28 11:15:19 -0300

    Muevo de lugar calculadora.py y creo celsiusfahrenheit.py

---
+++ Funciones/celsiusfahrenheit.py
@@ -0,0 +1,7 @@
+def celsius_a_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+print(celsius_a_fahrenheit(0))    # Output: 32.0
+print(celsius_a_fahrenheit(100))  # Output: 212.0
+print(celsius_a_fahrenheit(-40))  # Output: -40.0
+
